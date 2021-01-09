f13349921b46c9595f56dc25d74b56ad5cd41dcc
Obada Qaf <obadou8114@gmail.com>
2021-01-09 13:20:54 +0200

    V4

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ welcome = """
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
   -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Nuha Team</title>
+  <title>Nuha Team 1111111111111111111111111111111111</title>
   <style>
   body {
     color: #FF5733;
